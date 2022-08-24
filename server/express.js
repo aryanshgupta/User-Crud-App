c6d4ae1eadf0752e92ee72abcbd7a6982158d2d3
A. Sarsilmaz <abdulmannan50@gmail.com>
2022-08-25 01:41:27 +0530

    mern-backend

---
+++ server/express.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compress from 'compression';
+import cors from 'cors';
+import helmet from 'helmet';
+
+
+// modules for server side rendering
+const CURRENT_WORKING_DIR = process.cwd();
+const app = express();
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compress());
+// secure apps by setting various HTTP headers
+app.use(helmet());
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
+
+
+app.get('/', (req, res) => {
+  res.send('Tested');
+ });
+
+export default app;
