6621b63e865f40277cfac2fc7de63eeb717685db
Aryansh Gupta <aryanshgupta@protonmail.com>
2022-10-13 17:41:04 +0530

    formatted

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,27 +1,27 @@
-import User from '../models/user.model';
-import extend from 'lodash/extend';
-import errorHandler from '../helpers/dbErrorHandler';
+import User from "../models/user.model";
+import extend from "lodash/extend";
+import errorHandler from "../helpers/dbErrorHandler";
 
 const create = async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
     return res.status(200).json({
-      message: "Successfully signed up!"
+      message: "Successfully signed up!",
     });
   } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
+      error: errorHandler.getErrorMessage(err),
     });
   }
 };
 const list = async (req, res) => {
   try {
-    let users = await User.find().select('name email updated created');
+    let users = await User.find().select("name email updated created");
     res.json(users);
   } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
+      error: errorHandler.getErrorMessage(err),
     });
   }
 };
@@ -29,14 +29,14 @@ const userByID = async (req, res, next, id) => {
   try {
     let user = await User.findById(id);
     if (!user)
-      return res.status('400').json({
-        error: "User not found"
+      return res.status("400").json({
+        error: "User not found",
       });
     req.profile = user;
     next();
   } catch (err) {
-    return res.status('400').json({
-      error: "Could not retrieve user"
+    return res.status("400").json({
+      error: "Could not retrieve user",
     });
   }
 };
@@ -46,7 +46,6 @@ const read = (req, res) => {
   req.profile.salt = undefined;
   return res.json(req.profile);
 };
-
 
 const update = async (req, res) => {
   try {
@@ -59,11 +58,10 @@ const update = async (req, res) => {
     res.json(user);
   } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
+      error: errorHandler.getErrorMessage(err),
     });
   }
 };
-
 
 const remove = async (req, res) => {
   try {
@@ -74,9 +72,9 @@ const remove = async (req, res) => {
     res.json(deletedUser);
   } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
+      error: errorHandler.getErrorMessage(err),
     });
   }
 };
 
-export default { create, userByID, read, list, remove, update }
+export default { create, userByID, read, list, remove, update };
--- template.js
+++ template.js
@@ -1,4 +1,4 @@
-export default ({markup, css}) => {
+export default ({ markup, css }) => {
   return `<!doctype html>
     <html lang="en">
       <head>
@@ -22,5 +22,5 @@ export default ({markup, css}) => {
         <style id="jss-server-side">${css}</style>
         <script type="text/javascript" src="/dist/bundle.js"></script>
       </body>
-    </html>`
+    </html>`;
 };
