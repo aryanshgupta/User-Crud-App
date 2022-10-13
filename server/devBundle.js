f5a934fb4608aa522eda55480f22b815e5e1a826
Aryansh Gupta <aryanshgupta@protonmail.com>
2022-10-13 17:17:48 +0530

    complete frontend and backend

--- server/devBundle.js
+++ server/devBundle.js
@@ -1,15 +1,15 @@
 import config from './../config/config';
-import webpack from 'webpack'
+import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
-i//port webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from './../webpack.config.client.js'
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from './../webpack.config.client.js';
 
 const compile = (app) => {
   if(config.env === "development"){
-    const compiler = webpack(webpackConfig);
+    const compiler = webpack(webpackConfig)
     const middleware = webpackMiddleware(compiler, {
       publicPath: webpackConfig.output.publicPath
-    });
+    })
     app.use(middleware);
     app.use(webpackHotMiddleware(compiler));
   }
